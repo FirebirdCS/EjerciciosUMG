84bf12b6c3ece356c5abecb6a5fc16817e7c0953
FirebirdCS <alvaritotutos@gmail.com>
2024-04-19 10:01:08 -0600

    Lab actualizado, parcial

--- Lab #3 - Arduino Interfaz Poten Led/ledArduino.py
+++ Lab #3 - Arduino Interfaz Poten Led/ledArduino.py
@@ -26,7 +26,7 @@ def handle_code(code):
     try:
         digit = int(code)
         print("Código recibido:", digit)
-        # Apagar todos los círculos primero
+        # Apagar todos los círculos y rectángulos primero
         for circle in circles:
             turn_off_circle(circle)
 
@@ -37,10 +37,24 @@ def handle_code(code):
             turn_on_circle(circles[1], "blue")
         elif digit == 3:
             turn_on_circle(circles[2], "yellow")
+        # Encender el cuadrado correspondiente al código recibido y su modo
+        if digit == 5:
+            canvas.create_rectangle(301, 150, 350, 200, outline="black", width=2, fill="red")
+        elif digit == 6:
+            canvas.create_rectangle(371, 150, 420, 200, outline="black", width=2, fill="red")
+        elif digit == 7:
+            canvas.create_rectangle(441, 150, 490, 200, outline="black", width=2, fill="red")
+        elif digit == 8:
+            canvas.create_rectangle(301, 150, 350, 200, outline="black", width=2, fill="white")
+            canvas.create_rectangle(371, 150, 420, 200, outline="black", width=2, fill="white")
+            canvas.create_rectangle(441, 150, 490, 200, outline="black", width=2, fill="white")
         # Actualizar el valor de la barra de potenciómetro
         update_bar_graph(digit)
     except ValueError:
-        print("Mensaje desde Arduino:", code)
+        print("Mensaje desde Arduino:", code.strip())
+
+
+
 
 # Función para actualizar la barra de potenciómetro
 def update_bar_graph(value):
@@ -72,9 +86,15 @@ bar = canvas.create_rectangle(50, 400, 50, 450, fill='green')
 # Configuración de los circulos
 circles = []
 for i in range(3):
-    circle = canvas.create_oval(50 + i * 70, 150, 100 + i * 70, 200, outline="black", width=2)
+    circle = canvas.create_oval(50 + i * 70, 150, 100 + i * 70, 200, outline="black", width=2, fill="white")
     canvas.create_text(75 + i * 70, 175, text=str(i+1), font=("Arial", 12))
     circles.append(circle)
+
+# Rectángulos
+rectangle1 = canvas.create_rectangle(301, 150, 350, 200, outline="black", width=2, fill="white")
+rectangle2 = canvas.create_rectangle(371, 150, 420, 200, outline="black", width=2, fill="white")
+rectangle3 = canvas.create_rectangle(441, 150, 490, 200, outline="black", width=2, fill="white")
+
 
 # Labels
 
@@ -85,8 +105,16 @@ title_text.place(x=250, y=25)
 button_text = tk.Label(root, text="LEDs")
 button_text.place(x=50, y=100)
 
+button_text = tk.Label(root, text="Modo")
+button_text.place(x=300, y=100)
+
 pot_text = tk.Label(root, text="Potenciómetro")
 pot_text.place(x=50, y=250)
+
+
+canvas.create_text(325, 210, text="Inorden", font=("Arial", 11))
+canvas.create_text(390, 210, text="Postorden", font=("Arial", 11))
+canvas.create_text(470, 210, text="Preorden", font=("Arial", 11))
 
 # Crear y ejecutar el hilo para leer datos del puerto serie
 serial_thread = threading.Thread(target=serial_reader)
